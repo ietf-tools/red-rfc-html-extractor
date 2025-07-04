71f4cb2cb9589066dca52f3c080dedb11a6a7b31
Matthew Holloway <matthew@holloway.co.nz>
2025-07-04 14:08:14 +1200

    fix: fixing TOC scraping

--- src/index.test.ts
+++ src/index.test.ts
@@ -5,9 +5,17 @@ import { processRfcBucketHtml } from './index'
 const RFC_WITH_PREFORMATTED_HTML = 1234
 const RFC_WITH_HTML = 9000
 
-test('processRfcBucketHtml()', async () => {
+test(`processRfcBucketHtml(${RFC_WITH_PREFORMATTED_HTML}) RFC without TOC`, async () => {
+  const rfcBucketHtmlDocument = await processRfcBucketHtml(RFC_WITH_PREFORMATTED_HTML)
   expect(
-    await processRfcBucketHtml(RFC_WITH_PREFORMATTED_HTML)
+    rfcBucketHtmlDocument
   ).toMatchSnapshot()
-  expect(await processRfcBucketHtml(RFC_WITH_HTML)).toMatchSnapshot()
+
+  expect(rfcBucketHtmlDocument.tableOfContents).toBeUndefined()
+})
+
+test(`processRfcBucketHtml(${RFC_WITH_HTML})  RFC with TOC`, async () => {
+  const rfcBucketHtmlDocument = await processRfcBucketHtml(RFC_WITH_HTML)
+  expect(rfcBucketHtmlDocument).toMatchSnapshot()
+  expect(rfcBucketHtmlDocument.tableOfContents).toBeTruthy()
 })
