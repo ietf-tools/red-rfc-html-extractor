02203b921c0817145fe80340e2e45eb401b8b797
NGPixel <github@ngpixel.com>
2025-07-09 04:27:43 -0400

    fix: add minRfcNumber to all.ts

--- src/all.ts
+++ src/all.ts
@@ -2,8 +2,8 @@ import { fetchSourceRfcHtml, rfcBucketHtmlToRfcDocument } from './utils.ts'
 import { saveToS3 } from './s3.ts'
 import { setTimeout } from 'node:timers/promises'
 
-const main = async (maxRfcNumber: number): Promise<void> => {
-  for (let rfcNumber = 1; rfcNumber <= maxRfcNumber; rfcNumber++) {
+const main = async (minRfcNumber: number, maxRfcNumber: number): Promise<void> => {
+  for (let rfcNumber = minRfcNumber; rfcNumber <= maxRfcNumber; rfcNumber++) {
     console.log(`Processing RFC ${rfcNumber}...`)
     try {
       const html = await fetchSourceRfcHtml(rfcNumber)
@@ -15,15 +15,15 @@ const main = async (maxRfcNumber: number): Promise<void> => {
         console.log(`Failed to fetch RFC ${rfcNumber}, skipping...`)
         continue
       }
-      await setTimeout(100)
+      await setTimeout(80)
     } catch (err) {
       console.warn(`Failed to process ${rfcNumber}: ${(err as Error).message}`)
     }
   }
 }
 
-if (!process.argv[2]) {
-  throw Error(`Script requires max RFC Number arg but argv was ${JSON.stringify(process.argv)}`)
+if (!process.argv[2] || !process.argv[3]) {
+  throw Error(`Script requires min and max RFC Number args but argv was ${JSON.stringify(process.argv)}`)
 }
 
-main(parseInt(process.argv[2], 10))
+main(parseInt(process.argv[2], 10), parseInt(process.argv[3], 10))
